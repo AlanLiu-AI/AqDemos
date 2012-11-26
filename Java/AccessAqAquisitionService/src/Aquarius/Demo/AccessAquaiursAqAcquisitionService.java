8169277058c6c1392a80a9b2071f00037667c486
Alan Liu <alan.liu@aquaticinformatics.com>
2012-11-26 15:41:15 -0800

    For Java Soap Client: Referred AuthToken as a Soap Header to call GetAllLocation.

--- Java/AccessAqAquisitionService/src/Aquarius/Demo/AccessAquaiursAqAcquisitionService.java
+++ Java/AccessAqAquisitionService/src/Aquarius/Demo/AccessAquaiursAqAcquisitionService.java
@@ -1,11 +1,19 @@
 package Aquarius.Demo;
 
 import java.net.URL;
+import java.util.Iterator;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 
-import org.tempuri.*;
+import com.sun.xml.ws.developer.WSBindingProvider;
+import com.sun.xml.ws.api.message.Headers;
+
+import org.tempuri.AQAcquisitionService;
+import org.tempuri.IAQAcquisitionService;
+
+import org.datacontract.schemas._2004._07.aquariusdataservice.ArrayOfLocationDTO;
+import org.datacontract.schemas._2004._07.aquariusdataservice.LocationDTO;
 
 public class AccessAquaiursAqAcquisitionService {
 
@@ -23,13 +31,30 @@ public class AccessAquaiursAqAcquisitionService {
 			String autoToken = clientPort.getAuthToken("admin",  "admin");
 			System.out.println("AuthToken is: " + autoToken);
 			
+			WSBindingProvider bindingProvider = (WSBindingProvider) clientPort;
+			bindingProvider.setOutboundHeaders(
+			        Headers.create(new QName("AQAuthToken"), autoToken)
+			);
+			ArrayOfLocationDTO allLocations = clientPort.getAllLocations();
+			System.out.println("There are total " + allLocations.getLocationDTO().size() + " Locations: ");
+			Iterator<LocationDTO> iter = allLocations.getLocationDTO().iterator(); 
+			while(iter.hasNext()) {
+				LocationDTO location = iter.next(); 
+				System.out.println("\t" + location.getIdentifier());
+			} 
 			
+			bindingProvider.close();//Avoid resource leaking
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
-		}	
-
+		}
+		
+		System.out.println("Enter to quit...");
+		try {
+	        System.in.read();
+	    } catch (Exception e) {
+	        System.out.println(e.getMessage());
+	    }
 	}
-
 }
